1078434b858f479569370d7172c05ae4496ed1ee
Michael Martin <michael.martin@life.church>
2018-06-03 00:18:39 -0500

    WIP Cloud Functions to Auto-Calc Summary Values

--- client/functions/index.js
+++ client/functions/index.js
@@ -1,50 +1,111 @@
+/* eslint-disable no-console */
 const functions = require('firebase-functions')
+
+function initializeSummaryObject(summary) {
+  return Object.assign({}, {
+    frames: 0,
+    styles: 0,
+    value: 0,
+  }, summary)
+}
+
+function updateSummary(inventory = {}, before = {}, after = {}) {
+  const {
+    fifo, mims, diff, counts,
+  } = inventory
+
+  const newFifo = initializeSummaryObject(fifo)
+  const newMims = initializeSummaryObject(mims)
+  const newDiff = initializeSummaryObject(diff)
+  const newCounts = Object.assign({}, counts)
+
+  if (after.scannedQty > after.reportQty) {
+    newCounts.over += 1
+  } else if (after.scannedQty < after.reportQty) {
+    newCounts.under += 1
+  } else {
+    newCounts.even += 1
+  }
+
+  if (before.scannedQty > before.reportQty) {
+    newCounts.over -= 1
+  } else if (before.scannedQty < before.reportQty) {
+    newCounts.under -= 1
+  } else {
+    newCounts.even -= 1
+  }
+
+  if (after.scannedQty > 0 && before.scannedQty <= 0) newFifo.styles += 1
+  if (after.reportQty > 0 && before.reportQty <= 0) newMims.styles += 1
+
+  newFifo.frames -= before.scannedQty
+  newFifo.frames += after.scannedQty
+
+  newFifo.value -= before.sellInPrice
+  newFifo.value += after.sellInPrice
+
+  newMims.frames -= before.reportQty
+  newMims.frames += after.reportQty
+
+  newMims.value -= before.sellInPrice
+  newMims.value += after.sellInPrice
+
+  newDiff.frames = newMims.frames - newFifo.frames
+  newDiff.styles = newMims.styles - newFifo.styles
+  newDiff.value = newMims.value - newFifo.value
+
+  return {
+    fifo: newFifo,
+    mims: newMims,
+    diff: newDiff,
+    counts: newCounts,
+  }
+}
 
 exports.calculateInventorySummaryFields = functions.firestore
   .document('/organizations/{organizationId}/inventories/{inventoryId}/counts/{countId}')
-  .onWrite((change, context) => {
-    // const { inventoryId } = context.params
+  .onWrite((change) => {
+    if (!change.after.exists) return
 
-    // const before = change.before.data()
-    const after = change.after.exists ? change.after.data() : null
+    const { before, after } = change
 
-    const inventoryRef = change.after.parent().parent()
+    const beforeData = before.data()
+    const afterData = after.data()
+
+    console.log('b/a:', before, after)
+    console.log('b/a data:', beforeData, afterData)
+
+    const inventoryRef = change.after.ref.parent.parent
+
+    console.log('Inventory Ref: ', inventoryRef)
 
     return change.after.ref.firestore.runTransaction((transaction) => {
+      console.log('Running Transaction: ', transaction)
+
       // This code may get re-run multiple times if there are conflicts.
-      transaction.get(inventoryRef).then((inventoryDoc) => {
+      return transaction.get(inventoryRef).then((inventoryDoc) => {
         if (!inventoryDoc.exists) {
           throw new Error('Document does not exist!')
         }
 
-        const { counts } = inventoryDoc.data()
-        if (after.scannedQty > after.reportQty) {
-          counts.over += 1
-        } else if (after.scannedQty < after.reportQty) {
-          counts.under += 1
-        } else {
-          counts.even += 1
-        }
+        console.log('Inventory Doc: ', inventoryDoc)
 
-        transaction.update(inventoryRef, { counts })
+        const inventoryData = inventoryDoc.data()
+
+        console.log('Inventory Data: ', inventoryData)
+
+        const updatedInventory = Object.assign(
+          {},
+          updateSummary(inventoryData, beforeData, afterData),
+        )
+
+        console.log('Updated: ', updatedInventory)
+
+        transaction.update(inventoryRef, updatedInventory)
       })
     }).then(() => {
       console.log('Transaction successfully committed!')
     }).catch((error) => {
       console.log('Transaction failed: ', error)
     })
-
-    // If we set `/users/marie/incoming_messages/134` to {body: "Hello"} then
-    // context.params.userId == "marie";
-    // context.params.messageCollectionId == "incoming_messages";
-    // context.params.messageId == "134";
-    // ... and ...
-    // change.after.data() == {body: "Hello"}
   })
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
